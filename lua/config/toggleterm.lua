2612f62d41721c038c77f014fec09a39a71a9cad
Atharv Maan <atharvmaan@chromium.org>
2025-11-26 15:46:34 -0500

    add command to autotest with gtest filter

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -7,14 +7,27 @@ toggleterm.setup({
   shell = "pwsh.exe", -- Use your default shell (likely Zsh, based on your interests)
 })
 
+function get_symbol_under_cursor()
+  return vim.fn.expand('<cword>')
+end
+
 -- Autotest current file in toggleterm
-function run_autotest_on_current_file()
+function run_autotest_on_current_file(symbol)
   local file_path = get_current_file_relative_path()
 
   local command = "vpython3 tools/autotest.py -C out/Default " .. file_path
 
+  if symbol then
+    command = command .. " --gtest_filter=*" .. symbol .. "*"
+  end
+
   -- Open ToggleTerm and execute the command
   toggleterm.exec(command, 1, 2, nil, 'float')
+end
+
+function run_test_on_current_file_filtered_on_symbol()
+  local symbol = get_symbol_under_cursor()
+  run_autotest_on_current_file(symbol)
 end
 
 function build_chrome() 
@@ -28,11 +41,22 @@ function run_chrome_with_default_args()
   toggleterm.exec(command, 1, 2, nil, 'float')
 end
 
-vim.keymap.set('n', '<leader>ct', run_autotest_on_current_file, {
+vim.keymap.set('n', '<leader>ctf', run_autotest_on_current_file, {
   noremap = true,
   silent = true,
   desc = 'Run autotest.py on current file'
 })
+
+vim.keymap.set(
+  'n',
+  '<leader>cts',
+  run_test_on_current_file_filtered_on_symbol,
+  {
+    noremap = true,
+    silent = true,
+    desc = 'Run autotest.py on current file filtered on symbol under cursor'
+  }
+)
 
 vim.keymap.set('n', '<leader>cb', build_chrome, {
   noremap = true,
